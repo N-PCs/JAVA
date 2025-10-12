d312be4e28cc23282a7e9b13389e7d84a39dc30e
Neel Pandey <neelpandeyofficial@gmail.com>
2025-10-12 21:08:24 +0530

    Update ConstructorOverloading.java

--- ConstructorOverloading.java
+++ ConstructorOverloading.java
@@ -1,30 +1,36 @@
 public class ConstructorOverloading {
     public int number;
     public String text;
+    
     // Default constructor
     public ConstructorOverloading() {
         number = 0;
         text = "default";
     }
+    
     // Constructor with number parameter
     public ConstructorOverloading(int num) {
         number = num;
         text = "numberOnly";
     }
+    
     // Constructor with text parameter
     public ConstructorOverloading(String txt) {
         number = 0;
         text = txt;
     }
+    
     // Constructor with both number and text parameters
     public ConstructorOverloading(int num, String txt) {
         number = num;
         text = txt;
     }
+    
     // Method to display object values
     public void display() {
         System.out.println("Number: " + number + ", Text: " + text);
     }
+    
     // Main method to test the class
     public static void main(String[] args) {
         ConstructorOverloading obj1 = new ConstructorOverloading();
@@ -36,4 +42,5 @@ public class ConstructorOverloading {
         obj3.display();
         obj4.display();
     }
+
 }
